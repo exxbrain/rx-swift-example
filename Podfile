28dfe86b2b3ef8e3437fdee183821ac727f53f3c
Denis Zakharov <exxbrain@gmail.com>
2019-10-05 12:50:48 +0300

    Initial commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '13.0'
+use_frameworks!
+
+target 'rx-swift-example' do
+  pod 'Alamofire', '~> 5.0.0-rc.2'
+  pod 'RxSwift', '~> 5'
+  pod 'RxCocoa', '~> 5'
+end
---
+++ rx-swift-exampleTests/rx_swift_exampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  rx_swift_exampleTests.swift
+//  rx-swift-exampleTests
+//
+//  Created by Denis Zakharov on 05/10/2019.
+//  Copyright © 2019 Denis Zakharov. All rights reserved.
+//
+
+import XCTest
+@testable import rx_swift_example
+
+class rx_swift_exampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
